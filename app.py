6e743b6ed0a47256a16480da9638733061438abe
Babu Lamani <work.babulamani@gmail.com>
2025-11-11 12:34:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from fastapi.staticfiles import StaticFiles
 import qrcode
 import io
 
-app = FastAPI(root_path="/qrcode")
+app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
